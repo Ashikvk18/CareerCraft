370ace95edf2fd7814cd8cd9a57119dd3fb45f77
Ashik Dey Rupak <129338031+Ashikvk18@users.noreply.github.com>
2026-03-12 11:06:25 -0500

    Add user registration API with bcrypt password hashing

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const bcrypt = require('bcrypt');
 const pool = require('./db');
 
 const PORT = 3000;
+app.use(express.json());
 app.get('/api/users', async (req, res) => {
     try {
         const result = await pool.query('SELECT * FROM users');
@@ -15,7 +17,22 @@ app.get('/api/users', async (req, res) => {
 app.get('/', (req, res) => {
     res.send('CarrerCraft backend is running');
 });
+app.post('/api/users', async (req, res) => {
+    try {
+        const { email, password, name} = req.body;
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+        const result = await pool.query( 'INSERT INTO users( email, password, name)  VALUES ($1, $2, $3) RETURNING *',
+            [email, hashedPassword, name]
+        );
+        res.json(result.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).json({ error: 'Server error' });
 
+    }
+});
+    
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
